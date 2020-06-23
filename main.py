5b445b9069e981dda4187eac8a842e75ffdcb234
MingLun Wu <allen6997535@gmail.com>
2020-06-23 17:16:12 +0800

    Create folder feature and Upload environment.yml.

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from crawler import Crawler_LTN, filter_keyword
 import pandas as pd
 import json 
 import datetime
+import os
 
+# Create output folder. (If not exist.)
+if not os.path.exists(os.path.join(os.getcwd(), "output")):
+    os.makedirs(os.path.join(os.getcwd(), "output"))
 # Crawler URL.
 YUNLIN = "https://news.ltn.com.tw/ajax/breakingnews/Yunlin/"
 POLITIC = "https://news.ltn.com.tw/ajax/breakingnews/politics/"
@@ -31,6 +35,10 @@ for target in CRAWL_TARGET:
     temp = crawler.call_api(target, start_date, end_date)
     result = pd.concat([result, temp], axis=0)
 
-result = filter_keyword(result, param['keyword'])
-result.to_csv(datetime.datetime.now().strftime("%y-%m-%d_%H_%M")+".csv")
+if param["keyword"] is None or len(param["keyword"])==0:
+    print("未設定關鍵字！共有{}則新聞".format(result.shape[0]))
+else:
+    result = filter_keyword(result, param['keyword'])
+
+result.to_csv(os.path.join(os.getcwd(), "output", datetime.datetime.now().strftime("%y_%m_%d_%H_%M")+".csv"))
 print("查詢程序完成！已儲存資料！")
